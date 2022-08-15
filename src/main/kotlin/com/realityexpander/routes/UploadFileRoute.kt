db2167452344f1bf206d9e92ca53923ff32bbcc9
Chris Athanas <realityexpander@gmail.com>
2022-08-15 04:20:46 -0500

    Change file serve folder

--- src/main/kotlin/com/realityexpander/routes/UploadFileRoute.kt
+++ src/main/kotlin/com/realityexpander/routes/UploadFileRoute.kt
@@ -7,14 +7,17 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 import kotlin.random.Random
 
+// Example ExtraJson class
 data class ExtraJson(
     val key: String
 )
 
 fun Route.uploadFile() {
 
+    // Upload an image
     post("image") {
         val multipart = call.receiveMultipart()
 
@@ -23,6 +26,7 @@ fun Route.uploadFile() {
                 is PartData.FormItem -> {
                     println("${part.name} ==> ${part.value}")
 
+                    // Show how to extract json from the form data
                     if(part.name == "extraJson") {
                         println("key = ${Gson().fromJson(part.value, ExtraJson::class.java).key}")
                     }
@@ -30,7 +34,10 @@ fun Route.uploadFile() {
                 is PartData.FileItem -> {
                     if(part.name == "image_file") {
                         println("part.originalFileName = ${part.originalFileName}")
-                        part.save("build/resources/main/static/images/", part.originalFileName ?: "image_file-${Random.nextLong().toString()}.jpg")
+                        part.save("build/resources/main/static/images/",
+                            part.originalFileName
+                                ?: "image_file-${Random.nextLong().toString()}.jpg"
+                        )
                     }
                 }
                 else -> Unit
@@ -38,5 +45,20 @@ fun Route.uploadFile() {
         }
 
         call.respond(HttpStatusCode.OK)
+    }
+
+    // Download an image
+    get("download") {
+        // get filename from request url
+        val filename = call.parameters["fileName"]!!
+
+        // construct reference to file
+        // ideally this would use a different filename
+        val file = File("filesToServe/$filename")
+
+        if(file.exists()) {
+            call.respondFile(file)
+        }
+        else call.respond(HttpStatusCode.NotFound)
     }
 }
